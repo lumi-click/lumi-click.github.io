8483d34b0ee5f21b94a1372aa6490281604118a7
Oleg Popadko <oleg@popadko.com>
2023-03-16 17:56:55 +0100

    Initial commit

---
+++ _next/static/UaMx3d2eZ01RoCWuK9oNl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/174-a4b63ca6bd18e74b.js","static/chunks/pages/index-9143b2b9af603353.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
